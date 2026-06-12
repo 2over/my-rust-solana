803882d4e102396900c970ccd90e32525766abf0
vinpink <mysunshine2126@gmail.com>
2026-06-12 17:21:30 +0800

    feat: solana智能合约之间的调用

--- client/client.ts
+++ client/client.ts
@@ -1,24 +1,37 @@
-// 使用Web3.Transaction()创建一个新的Tx
-const transaction = new web3.Transaction();
+// 发起调用的智能合约(中转合约)
+const MAIN_PROGRAM_ID = new web3.PublicKey('FNFYU1WUuQMVLHbVU891WMDptbEFZmEKwgdAvd9GD9HT')
 
-// 创建一个Instruct
-// Keys[]中列出了交易中涉及的所有账户和它们各自的访问权限
-//(例如, 是否可以读取、写入等)，因为代码不需要与用户账户交互，
-// 所以这里keys[]为空
+
+// 目标智能合约(Hello Web3 Solana)
+const HELLO_PROGRAM_ID = new web3.PublicKey('GeJHumojz9pD97fJC76mrWpbNEBWKHFXAjoY1TXQhD38');
+
+
+ // 创建一个新交易
+ const transaction = new web3.Transaction();
+
+// 创建一个指令，发送给MAIN_PROGRAM_ID发起调用的合约
 transaction.add(
   new web3.TransactionInstruction({
-    keys: [],
-    programId: new web3.PublicKey(pg.PROGRAM_ID),
-  }),
+    programId: MAIN_PROGRAM_ID,
+    keys: [
+      // isSinger 标识该账户是否作为交易的签名者
+      // isWritable 表示该账户是否是可写的，可写代表可修改
+
+      // 发起调用的个人账户
+      {pubkey:pg.wallet.keypair.publicKey, isSigner: true, isWritable: true},
+      // 要调用的智能合约
+      { pubkey:HELLO_PROGRAM_ID, isSigner: false, isWritable: false},
+    ],
+    data: Buffer.alloc(0), // 传递给合约的数据
+  })
 );
 
-
-// 调用sendAndConfirmTransaction()方法, 参数为端口、交易、签名的私钥数组
+// 发起并确认交易
 const txHash = await web3.sendAndConfirmTransaction(
   pg.connection,
   transaction,
-  [pg.wallet.keypair],
+  [pg.wallet.keypair]
 );
 
-// 打印消息记录到控制台， 并显示哈希值
-console.log("Transaction sent with hash:", txHash);
+// 打印交易哈希
+console.log("交易哈希:", txHash)
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,73 @@
-// 导入solana-program
-// Account_info: 账户详细信息
-// entrypoint 程序入口
-// msg: 在Solana上打印消息
-use solana_program:: {
+use std::slice::Iter;
+
+use solana_program::{
     account_info::AccountInfo,
     entrypoint,
     entrypoint::ProgramResult,
+    msg,
     pubkey::Pubkey,
-    msg
+    program::{invoke},
+    instruction::{AccountMeta, Instruction},
+    program_error::ProgramError,
 };
 
-// 定义代码的入口,开始执行智能合约
+
+// 定义代码的入口, process_instruction是程序启动时被调用的函数
 entrypoint!(process_instruction);
 
-
-// 定义一个名为process_instruction的公共函数, 
-// 参数为程序id、账户和指令数据字段
+// 参数为当前程序的id、传入的账户数组 以及传入的指令数据字段
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
-) -> ProgramResult{
-    // 将""Hello Solana"消息打印在Solana区块链上
-    msg!("Hello Web3 Solana!");
+    _instruction_data: &[u8]
+) -> ProgramResult {
 
-    // 像系统返回状态代码来退出程序
+    // 获取传入账户数组的迭代器
+    let accounts_iter: &mut Iter<AccountInfo> = &mut accounts.iter();
+
+    // 获取调用该程序的账户
+    let caller_account: &AccountInfo = accounts_iter.next().ok_or_else(|| {
+        msg!("Failed to get caller account");
+        ProgramError::NotEnoughAccountKeys
+    })?;
+
+    // 被调用的账户程序
+    let called_program_account: &AccountInfo = accounts_iter.next().ok_or_else(|| {
+        msg!("Failed to get called program account");
+        ProgramError::NotEnoughAccountKeys
+    })?;
+
+    // 创建指向"Hello, Web3 Solana" 合约的指令
+    let instruction: Instruction = Instruction {
+        program_id: *called_program_account.key,
+        accounts: vec![
+            // true标识需要签名 new_readonly 只读
+            AccountMeta::new(*caller_account.key, true),
+            AccountMeta::new_readonly(*called_program_account.key, false),
+
+        ],
+        data: vec![], // 这里可以传递给被调用程序的数据
+    };
+
+    // 调用""Hello Web3 Solana" 合约
+    invoke(
+        &instruction,
+        // 再传入一边地址，顺序与instruction中的accounts账户顺序一致
+        &[
+            // 签名者
+            caller_account.clone(),
+            // 被调用的程序
+            called_program_account.clone(),
+        ],
+    )?;
+
+
+    // 打印消息
+    msg!("调用合约成功");
+    msg!("中转合约的地址program_id是{}", program_id.to_string());
+    msg!("发起调用的用户账户 caller_account 是{}", caller_account.key.to_string());
+    msg!("最终调用的目标地址 called_program_account 是:{}", called_program_account.key.to_string());
+
+    // 返回成功
     Ok(())
 }
